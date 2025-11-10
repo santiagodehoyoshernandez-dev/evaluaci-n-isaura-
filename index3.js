ade581e62a4f0ed90232057def62200d0b5f60a1
dilwil11 <dilanjwilchesh@gmail.com>
2025-11-10 11:28:37 -0500

    punto 3

---
+++ index3.js
@@ -0,0 +1,9 @@
+let num1=Number(prompt("dijite numero 1:"));
+let num2=Number(prompt("dijite numero 1:"));
+function tabla(num1,num2){
+let res=0
+    for(i=1;i>11;i++){
+        res=num1*num2;
+        console.log(num1+"*"+num2+"="+res);
+    }
+}
