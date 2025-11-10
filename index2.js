bc6e2ec36d6a4620b709ac927e6084d229441ab7
dilwil11 <dilanjwilchesh@gmail.com>
2025-11-10 11:23:58 -0500

    punto 2

---
+++ index2.js
@@ -0,0 +1,8 @@
+let num1=Number(prompt("dijite numero 1:"));
+let num2=Number(prompt("dijite numero 2:"));
+let res=0;
+function multiplicacion(num1,num2){
+    res=num1*num2;
+    return res;
+}
+console.log(res);
