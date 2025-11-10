b70fc46c2e7df4cd44b33b199c94224455ec60f7
Saraycastiblanco <sarayvioletacastiblanco20@gmail.com>
2025-11-10 11:52:48 -0500

    segundo ejercicio

---
+++ ejercicio2.py
@@ -0,0 +1,17 @@
+/*elaborar un algortimo que muestre el total pagado de una compra con x cantidad de productos. aparte
+del total debe mostrar el iva con descuento del 5% si la compra es superior 100k pesos, la 
+persona paga en efectivo por ende debe de mostrar cuales son las
+vueltas del usuario*/
+
+input "digite la cantidad de productos ",a;
+input "digite el total de su compra", b;
+input "digite la cantidad de pago en efectivo",f;
+
+if (a >= 100000) AND ("f")
+  multi = a - 0.5;
+  resultado = f - multi;
+return f;
+else 
+ input "no tiene descuento"; 
+
+ 
